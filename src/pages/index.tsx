916087557a07e7a3761bcebc3ffeb344349bed23
EthanChenYen-Peng <ypcethan@gmail.com>
2022-02-28 18:41:35 +0800

    move homepage component to its own dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,1 @@
-import type { NextPage } from "next";
-
-const Home: NextPage = () => {
-  return (
-    <>
-      Hello
-    </>
-  );
-};
-
-export default Home;
+export { default } from '@/pages/home'
