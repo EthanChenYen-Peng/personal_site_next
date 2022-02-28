0c945b56cfc8f02373f13e355c94ab8e2a3d0b4d
EthanChenYen-Peng <ypcethan@gmail.com>
2022-02-28 15:18:11 +0800

    add background overlay

--- src/components/navigation/MobileNav/MobileNav.styled.ts
+++ src/components/navigation/MobileNav/MobileNav.styled.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { sizes } from '@/utils/constants'
 
 export const Wrapper = styled.div`
   display: none;
-  @media (max-width: 600px) {
+  @media (max-width: ${sizes.sm}) {
+    display: block;
     position: fixed;
     background-color: var(--primary-color);
     color: var(--secondary-color);
@@ -10,7 +12,7 @@ export const Wrapper = styled.div`
     height: calc(100vh - 4rem);
     right: 0;
     bottom: 0;
-    transform: ${(props) => (!props.open ? "translateX(300px)" : "")};
+    transform: ${(props) => (props.open ? "" : "translateX(300px)")};
     transition: transform 0.5s;
 
     display: flex;
@@ -18,5 +20,20 @@ export const Wrapper = styled.div`
     justify-content: space-between;
     align-items: center;
     padding: 4rem 0;
+  }
+`;
+
+export const Overlay = styled.div`
+  display: none;
+  @media (max-width: ${sizes.sm}) {
+    display: block;
+    background-color: rgba(90, 90, 90, 0.6);
+    position: fixed;
+    height: calc(100vh - 4rem);
+    bottom: 0;
+    left: 0;
+    right: 0;
+    transform: ${(props) => (props.open ? "" : "translateX(600px)")};
+    transition: transform 0.7s;
   }
 `;
--- src/components/navigation/MobileNav/MobileNav.tsx
+++ src/components/navigation/MobileNav/MobileNav.tsx
@@ -1,10 +1,14 @@
-import { Wrapper } from "./MobileNav.styled";
+import { Wrapper, Overlay } from "./MobileNav.styled";
 interface Props {
   open: Boolean;
   children: any;
 }
 function MobileNav({ open, children }: Props) {
-  return <Wrapper open={open}>{children}</Wrapper>;
+  return (
+    <Overlay open={open}>
+      <Wrapper open={open}>{children}</Wrapper>
+    </Overlay>
+  );
 }
 
 export default MobileNav;
