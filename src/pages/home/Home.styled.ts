2bb3ffaff004c7b994a7235f574c712134913b86
EthanChenYen-Peng <ypcethan@gmail.com>
2022-03-02 10:10:11 +0800

    home page style

---
+++ src/pages/home/Home.styled.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+import { sizes } from '@/utils/constants'
+
+export const Container = styled.div`
+  background-color: var(--secondary-color);
+  color: var(--primary-color);
+  min-height: 100vh;
+
+
+`
+
+export const HeroContainer = styled.div`
+  font-size: 4rem;
+  padding: 5rem 7rem;
+
+  h2:nth-of-type(2) {
+    transform: translateX(200px);
+    margin-top: 2rem;
+  }
+
+  @media(max-width: ${sizes.sm} ) {
+    font-size: 2rem;
+    padding: 2rem 1rem;
+
+    h2:nth-of-type(2) {
+      transform: translateX(0px);
+    }
+  }
+`
+
+export const Highlight = styled.span`
+  position: relative;
+  padding: 0.5rem;
+  color: var(--secondary-color);
+
+  white-space: nowrap;
+  &::before {
+    position: absolute;
+    z-index: -1;
+    content: '';
+    width: 0%;
+    transform: rotate(1deg);
+    height: 100%;
+    background-color: var(--primary-color);
+    animation-name: drawHightlight;
+    animation-duration: 2s;
+    animation-delay: 1s;
+    animation-fill-mode: forwards;
+  }
+
+
+  @media(max-width: ${sizes.sm} ) {
+
+  }
+  @keyframes drawHightlight {
+    from {
+      width: 0%;
+    }
+
+    to {
+      width: 100%;
+    }
+  }
+`
+
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
+import { Container, HeroContainer, Highlight } from './Home.styled'
 
 const Home: NextPage = () => {
   return (
-    <>
-      Home
-    </>
+    <Container>
+      <HeroContainer>
+        <h2>Hi there, I'm Ethan</h2>
+        <h2>
+          I am a
+          <Highlight>web developer</Highlight>
+        </h2>
+      </HeroContainer>
+    </Container>
   );
 };
 
