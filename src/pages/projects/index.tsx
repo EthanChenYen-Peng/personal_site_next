28ed7b043b4cb0697a70fdf857d41d89024419f3
EthanChenYen-Peng <ypcethan@gmail.com>
2022-03-03 09:21:16 +0800

    posts api

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,6 +1,16 @@
+import { getAllPosts } from '@/api/posts';
 function Projects() {
   return (
       <div>Projects</div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = getAllPosts()
+    .slice(0, 9)
+    .map((post) => post.meta);
+  console.log(posts)
+
+  return { props: { posts } };
 }
 export default Projects
