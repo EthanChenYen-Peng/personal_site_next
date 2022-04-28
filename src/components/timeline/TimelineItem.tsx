2a73ad3b449db97f07ffc7bc1f940bc93be4d585
EthanChenYen-Peng <ypcethan@gmail.com>
2022-04-28 20:34:40 +1000

    add basic animation

--- src/components/timeline/TimelineItem.tsx
+++ src/components/timeline/TimelineItem.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 interface Props {
   title: string
@@ -21,33 +21,68 @@ const Container = styled.div`
   width: 100%;
 `
 
+const sidelineHeight = keyframes`
+  0% {
+    height: 0px;
+  }
+
+  100% {
+    height: 280px;
+  }
+}
+`
+
+const showCircle = keyframes`
+  0% {
+    width: 0px;
+    height: 0px;
+  }
+
+  100% {
+    width: 30px;
+    height: 30px;
+  }
+}
+`
 const SideLine = styled.div`
   background-color: var(--primary-color);
   width: 10px;
   position: relative;
+  height: 0px;
 
+  animation: ${sidelineHeight} 0.5s 1s ease-in both;
   &:after {
     content: '';
     position: absolute;
     bottom: 0;
-    width: 30px;
-    height: 30px;
+    width: 0px;
+    height: 0px;
     background: inherit;
-    transform: translateX(-33%);
+    transform: translateX(-32%);
     border-radius: 50%;
+    animation: ${showCircle} 0.3s 1.5s linear both;
   }
 `
+const showContent = keyframes`
+  0% {
+    opacity: 0;
+    width: 50%;
+  }
 
+  100% {
+    opacity: 1;
+    width: 100%;
+  }
+`
 const ContentContainer = styled.div`
   width: 100%;
   background: var(--primary-color);
   color: var(--secondary-color);
-  transition: all 0.5s ease-in-out;
   border-radius: 10px;
   margin-left: 4rem;
   margin-top: 4rem;
   padding: 2rem;
-  min-height: 200px;
+  animation: ${showContent} 0.5s 1s ease-in-out both;
 `
 
 const Content = styled.div`
