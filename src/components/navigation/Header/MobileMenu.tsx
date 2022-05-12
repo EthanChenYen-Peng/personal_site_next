a4680166faa1d14d2a573948d5c484c4b5e10b15
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-12 15:24:12 +1000

    remove unnecessary div

--- src/components/navigation/Header/MobileMenu.tsx
+++ src/components/navigation/Header/MobileMenu.tsx
@@ -42,7 +42,7 @@ function NavLinkItem({
       }}
       key={title}
       onClick={closeMenu}
-      className="relative  before:absolute before:left-0 before:-bottom-1 before:h-[3px] before:w-0 before:bg-secondary before:transition-all before:duration-200 before:content-[''] hover:before:w-full"
+      className="relative  before:absolute before:left-0 before:-bottom-1 before:h-[3px] before:w-0 before:bg-secondary before:transition-all before:duration-200 before:content-[''] hover:before:w-full focus:outline-none "
     >
       <Link href={path}>{title}</Link>
     </motion.li>
@@ -53,32 +53,31 @@ export default function MobileMenu({ isOpen, closeMenu, navLinks }: Props) {
     <AnimatePresence>
       {isOpen && (
         <DialogOverlay className="fixed inset-0 bg-black/30">
-          <DialogContent className="fixed inset-0 flex justify-end">
-            <MotionDialogContent
-              initial={{ translateX: '100%' }}
-              animate={{ translateX: '0px' }}
-              exit={{ translateX: '100%' }}
-              transition={{
-                ease: 'easeOut',
-                duration: 0.3,
-                when: 'beforeChildren',
-                staggerChildren: 0.3,
-              }}
-              className="flex h-full w-3/5 flex-col justify-center bg-primary text-secondary"
-            >
-              <ul className="flex flex-col items-start gap-12 pl-10 text-2xl">
-                {navLinks.map(({ path, title }, i) => (
-                  <NavLinkItem
-                    key={title}
-                    path={path}
-                    title={title}
-                    index={i}
-                    closeMenu={closeMenu}
-                  />
-                ))}
-              </ul>
-            </MotionDialogContent>
-          </DialogContent>
+          <MotionDialogContent
+            initial={{ translateX: '100%' }}
+            animate={{ translateX: '0px' }}
+            exit={{ translateX: '100%' }}
+            transition={{
+              ease: 'easeOut',
+              duration: 0.3,
+              when: 'beforeChildren',
+              staggerChildren: 0.3,
+            }}
+            aria-label="Mobie menu"
+            className="fixed inset-y-0 right-0 flex  w-3/5 flex-col justify-center bg-primary text-secondary"
+          >
+            <ul className="flex flex-col items-start gap-12 pl-10 text-2xl">
+              {navLinks.map(({ path, title }, i) => (
+                <NavLinkItem
+                  key={title}
+                  path={path}
+                  title={title}
+                  index={i}
+                  closeMenu={closeMenu}
+                />
+              ))}
+            </ul>
+          </MotionDialogContent>
         </DialogOverlay>
       )}
     </AnimatePresence>
